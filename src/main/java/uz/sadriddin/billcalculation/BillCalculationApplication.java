c768b319bbbfd9fac52f8f8fad10c9a5c2497afb
ssuyarov <s.suyarov@smartbank.uz>
2023-11-21 18:11:36 +0500

    changes:  - project initialized with needed dependencies;

---
+++ src/main/java/uz/sadriddin/billcalculation/BillCalculationApplication.java
@@ -0,0 +1,13 @@
+package uz.sadriddin.billcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillCalculationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/sadriddin/billcalculation/BillCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sadriddin.billcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
